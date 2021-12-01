fa5023ee605347cef9cfe7c36e476d7783beb567
Matt Zimmer <zimmermja@gmail.com>
2021-12-01 23:00:30 +0000

    backend server setup in notes-backend-server folder; index.js and index.html files created and tested to ensure build is working properly; firstTwenty function tested in DOM

---
+++ bundle.js
@@ -0,0 +1,20 @@
+(() => {
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // lib_functions/firstTwenty.js
+  var require_firstTwenty = __commonJS({
+    "lib_functions/firstTwenty.js"(exports, module) {
+      var firstTwenty2 = (str) => {
+        return `${str.substring(0, 20)}${str.length < 20 ? "" : "..."}`;
+      };
+      module.exports = firstTwenty2;
+    }
+  });
+
+  // index.js
+  var firstTwenty = require_firstTwenty();
+  var profilePic = document.querySelector("#testp");
+  profilePic.innerText = firstTwenty("Supercalifragilisticexpialidocious");
+})();
--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = 3000;
+const firstTwenty = require("./lib_functions/firstTwenty.js");
 
-app.use(express.json());
-app.use(cors());
-
-let notes = [];
-
-app.get('/notes', (req, res) => {
-  res.send(JSON.stringify(notes));
-});
-
-app.post('/notes', (req, res) => {
-  const newNote = {
-    title: req.body.title,
-    content: req.body.content
-  };
-
-  notes.push(newNote);
-  res.send(JSON.stringify(newNote));
-});
-
-app.listen(port);
-
-console.log(`Server listening on http://localhost:${port}`);
+const profilePic = document.querySelector("#testp");
+profilePic.innerText = firstTwenty("Supercalifragilisticexpialidocious");
